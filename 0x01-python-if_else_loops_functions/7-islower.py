e76558717509f2327754d010ff20525b3fd578c3
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-03 12:05:14 -0800

    indentation fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-	if ord(c) < 91:
-		return False
-	elif ord(c) > 96:
-		return True
+    if ord(c) < 91:
+        return False
+    elif ord(c) > 96:
+        return True
