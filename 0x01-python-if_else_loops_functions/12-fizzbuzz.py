3f442ac8c41ab91e45facdf0344c6c70ccf7d371
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-03 13:24:07 -0800

    Hotfix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-	for i in range(101):
+	for i in range(1, 101):
 		if (i % 3 == 0 and i % 5 == 0):
-			print("FizzBuzz")
+			print("FizzBuzz", end=' ')
 		elif (i % 3 == 0):
-			print("Fizz")
+			print("Fizz", end=' ')
 		elif (i % 5 == 0):
-			print("Buzz")
+			print("Buzz", end=' ')
+		else:
+			print(n, end=' ')
