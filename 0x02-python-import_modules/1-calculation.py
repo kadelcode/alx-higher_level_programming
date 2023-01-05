915ab559c65282743a3673ce7fce0952efca05c5
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-05 05:42:30 -0800

    Hotfix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-print("{} + {} = {}".format(10,5,add(10,5)))
-print("{} - {} = {}".format(10,5,sub(10,5)))
-print("{} * {} = {}".format(10,5,mul(10,5)))
-print("{} / {} = {}".format(10,5,div(10,5)))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
