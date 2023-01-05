c6aa110a825fd85d43f0c18f31799afed39d6cb1
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-05 00:35:43 -0800

    import calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,5 @@
+from calculator_1 import add, sub, mul, div
+print("{} + {} = {}".format(10,5,add(10,5)))
+print("{} - {} = {}".format(10,5,sub(10,5)))
+print("{} * {} = {}".format(10,5,mul(10,5)))
+print("{} / {} = {}".format(10,5,div(10,5)))
