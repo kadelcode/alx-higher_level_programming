f7201d895d68e363f6ff8c0b8547f9b1fb70611b
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-06 23:44:27 -0800

    code correction check

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    length = len(my_list)
     if idx < 0:
-	return (None)
+        return (None)
+
+    length = len(my_list)
+
     if idx > length - 1:
-	return (None)
+        return (None)
+
     return(my_list[idx])
