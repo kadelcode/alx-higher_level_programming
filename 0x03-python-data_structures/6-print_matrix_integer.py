cd8a565ba000288758816ad5cd969542c7484d15
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-07 12:19:55 -0800

    code fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        for j in ranage(len(matrix[i])):
+        for j in range(len(matrix[i])):
             if j != 0:
                 print(" ", end='')
             print("{:d}".format(matrix[i][j]), end='')
