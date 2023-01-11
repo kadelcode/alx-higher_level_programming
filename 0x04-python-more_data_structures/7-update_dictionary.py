1b3864171f4991774a490729dc5237367cf93362
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-11 09:33:39 -0800

    code fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[str(key)] = value
-    else:
+    if key not in a_dictionary:
         a_dictionary[str(key)] = value
+
+    return(a_dictionary)
