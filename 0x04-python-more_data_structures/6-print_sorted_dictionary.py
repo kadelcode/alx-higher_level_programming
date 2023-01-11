48feecd88b8c0f694995ca31884174f6c645d09f
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-11 08:48:44 -0800

    pycodestyle fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    list_dic = list(a_dictionary) # convert the dictionary to a list of keys
+    list_dic = list(a_dictionary)  # convert the dictionary to a list of keys
+
     sort_list = sorted(list_dic)
 
     for i in sort_list:
