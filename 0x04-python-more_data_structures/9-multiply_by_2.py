b142285658885c5884b695a4252c8bbcea8ce385
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-11 11:06:51 -0800

    check code issues

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     for i in a_dictionary:
-        a_dictionary[i] = 2 * int(a_dictionary[i])
+        a_dictionary[i] *= 2
     return (a_dictionary)
