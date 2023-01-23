a3e4ddd6c9c2584a49a35a01b4cd2043630aa64d
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-23 06:26:36 -0800

    safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+	try:
+		for i in range(0, x):
+			print(my_list[i], end='')
+		print()
+
+	except Exception:
+		print()
+		print("Length of list exceeded!")
+
+	return (x)
