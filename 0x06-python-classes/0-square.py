382f9c8379a1097182817f2979d2a8c5b29fab5c
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-24 05:41:33 -0800

    code changes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 # 0-square.py
-"""
-    An empty class "Square" that defines a square
-
-"""
+"""An empty class "Square" that defines a square"""
 
 class Square:
     """Represent a square"""
