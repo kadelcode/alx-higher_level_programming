7392edfe73dd69f83b16b766001613b52ac04dec
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-29 08:39:33 -0800

    type fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,16 +10,17 @@ def add_integer(a, b=98):
     """A function that adds two(2) integers.
     Integers are of type int or float`
     """
-    if not isinstance(a, int) or not isinstance(a, float):
-        raise TypeError('a must be an integer')
-
-    if not isinstance(b, int) or not isinstance(b, float):
-        raise TypeError('b must be an integer')
 
     if type(a) == float:
         a = int(a)
 
+    if not isinstance(a, int):
+        raise TypeError('a must be an integer')
+
     if type(b) == float:
         b = int(b)
+
+    if not isinstance(b, int):
+        raise TypeError('b must be an integer')
 
     return (a + b)
