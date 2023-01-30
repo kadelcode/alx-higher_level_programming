e4b5dbd3d79f7c1a0f6036ef478dfeffeeae525e
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-30 05:07:18 -0800

    empty rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: 0-rectangle
+An empty class Rectangle.
+This class defines a rectangle
+"""
+
+
+class Rectangle():
+    """
+    Defines an empty rectangle
+    """
+    pass
