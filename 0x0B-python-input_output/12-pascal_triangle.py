1141808c89639fc96f28d5a906c9d754441b9e6e
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-07 10:02:14 -0800

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" pascal triangle module """
+
+
+def pascal_triangle(n):
+    """
+        Returns a list o lists of integers representing the 
+        Pascal's triangle of n.
+
+            Args:
+                n (int): Integer
+    """
+    r = []
+    prev = []
+    curr = []
+    curr_len = 0
+
+    while n > 0:
+        prev = curr
+        curr = curr + [1]
+        i = 1
+        while i < len(prev):
+            curr[i] = prev[i - 1] + prev[i]
+            i += 1
+        r.append(curr)
+        n -= 1
+
+    return r
