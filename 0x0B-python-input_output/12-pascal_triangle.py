9d5dbc84e626d54e54f3d1ab9accf24dd507a46d
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-07 11:39:42 -0800

    pycodestyle fix

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,7 +4,7 @@
 
 def pascal_triangle(n):
     """
-        Returns a list o lists of integers representing the 
+        Returns a list o lists of integers representing the
         Pascal's triangle of n.
 
             Args:
