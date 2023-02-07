9283e9456d79b5c3d60adf37ef0ded22cb05b42a
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-06 22:52:47 -0800

    Square1

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""A class ```Square``` that inherits from Rectangle class"""
+
+Rectangle = __import__('10-square.py').Rectangle
+
+
+class Square(Rectangle):
+    """Child class for ```Rectangle```"""
+    def __init__(self, size):
+        """Initializes the params"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """Prints the desc of the `Rectangle`"""
+        string = "[Square] {}/{}".format(self.__size, self.__size)
+        return string
