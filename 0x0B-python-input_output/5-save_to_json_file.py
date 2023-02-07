1a87dcaedbe7c3329b9e2510e4aec8784cca5459
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-07 06:45:49 -0800

    save to json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" save to json file """
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes an object to a text file, using JSON representation """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
