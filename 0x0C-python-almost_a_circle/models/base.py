e79faa90be77960266839de8a028529159ad70d8
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-10 03:39:10 -0800

    base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Base class module"""
+
+
+class Base:
+    """A base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize class attribute"""
+        if id is not None:
+            self.id = id
+        self.__nb_objects += 1
