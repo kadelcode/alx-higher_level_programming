3b90dca8203b5748d6bc5d1f055a0cd78e866f09
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-10 06:14:04 -0800

    code fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,5 +11,5 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            self.__nb_objects += 1
-            self.id = id
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
