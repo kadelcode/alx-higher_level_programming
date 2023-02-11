672930f1420f8f7ed67d0dd792296bcdd9bd0ad1
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-11 12:25:10 -0800

    update code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Rectangle class module"""
 
+from models.base import Base
+
+
 class Rectangle(Base):
     """A Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
