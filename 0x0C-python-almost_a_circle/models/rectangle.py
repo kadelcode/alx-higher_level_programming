8ade91f018ba12c5389f4c1d30e6774d87d2947c
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-12 12:53:46 -0800

    non-keyword variable arguments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,17 @@ class Rectangle(Base):
     def __str__(self):
         """ returns the string representation of the rectangle """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        if args:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.__width = arg
+                if i == 2:
+                    self.__height = arg
+                if i == 3:
+                    self.__x = arg
+                if i == 4:
+                    self.__y = arg
