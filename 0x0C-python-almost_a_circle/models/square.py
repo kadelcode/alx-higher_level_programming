83898eeeb498b6b6f97a74c71ca8c528b49f19c1
Ayodele Kadiri <kadelcode@gmail.com>
2023-02-12 15:56:58 -0800

    getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,3 +9,19 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Initializes class attributes """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """get the size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets the size"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
