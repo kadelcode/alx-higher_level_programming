c4b384f4966e6d6b8f9c68a974690d3b97342d99
Ayodele Kadiri <kadelcode@gmail.com>
2023-03-15 07:24:14 -0700

    update code

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
 -- script that ceates the database `hbtn_0d_usa` and the table `cities` (in the db `hbtn_0d_usa`)
-CREATE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE hbtn_0d_usa.cities (
-	`id` INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	`id` INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 	`state_id` INT NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id)
